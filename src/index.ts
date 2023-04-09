01a889ba7611719d8790ada045100bb693b076e1
Ivan Garcia Gonzalez <alu0101388786@ull.edu.es>
2023-04-09 11:54:57 +0000

    ejercicio 2

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Función simple
- * @param cadena Cadena de texto
- * @returns la cadena de texto
- */
-export function holaMundo(cadena: string) {
-  return cadena;
-}
--- tests/index.spec.ts
+++
@@ -1,9 +0,0 @@
-import 'mocha'
-import { expect } from "chai";
-import { holaMundo } from "../src/index";
-
-describe('Tests para holaMundo', () => {
-  it('Prueba de funcion', () => {
-    expect(holaMundo('Hola Mundo')).to.be.eql('Hola Mundo');
-  })
-})
